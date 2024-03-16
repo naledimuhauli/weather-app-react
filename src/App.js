38baab523eafa0997e56f49dd037bdbd3d6484ac
naledimuhauli <naledimuhauli@gmail.com>
2024-03-16 23:57:53 +0200

    removed the image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  intgrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+  crossorigin="anonymous"
+/>;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="openn-source">
+          <div className="weather-appp">
+            <h1 id="city">city</h1>
+            <ul>
+              <li>
+                Last updated: <span id="currentDate">Thursday, 18:56</span>
+              </li>
+              <li id="description">clear</li>
+            </ul>
+            <div className="row">
+              <div className="col-6">
+                <h2>
+                  <div className="d-flex weather-temperature">
+                    <div className="d-flex weather-temperature">
+                      <strong id="temp">21°c</strong>
+                      <span className="units"></span>
+                    </div>
+                  </div>
+                </h2>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <div className="description">
+                    <li>
+                      Humidity: <span id="humidity">14</span>%
+                    </li>
+                    <li>
+                      Wind: <span id="wind">18</span>km/h
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+            <div className="weather-forecast" id="forecast"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Search from './Search';
+import Source from './Source';
+
+<link
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+rel="stylesheet"
+intgrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+crossorigin="anonymous"
+/>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <App />
+    <div className="weather-app">
+      <Search />
+      <App />
+    </div>
+    <Source />
+
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
